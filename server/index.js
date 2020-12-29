e4fb187850eccb6e7461fb43e803243018995359
Dean Vo <deaanvo@gmail.com>
2020-12-29 11:55:47 -0800

    changed express route so that database isn't involved and random restaurant is retrieved from /api/random route #1

--- server/index.js
+++ server/index.js
@@ -2,11 +2,6 @@ require('dotenv/config');
 const express = require('express');
 const staticMiddleware = require('./static-middleware');
 const fetch = require('node-fetch');
-const pg = require('pg');
-
-const db = new pg.Pool({
-  connectionString: process.env.DATABASE_URL
-});
 
 const app = express();
 
@@ -19,59 +14,24 @@ let selectedRestaurant = restaurantList;
 const term = 'restaurants';
 const location = 'orange county, ca';
 
-fetch(`https://api.yelp.com/v3/businesses/search?term=${term}&location=${location}`, {
-  headers: {
-    Authorization: process.env.BEARER
-  }
-})
-  .then(res => res.json())
-  .then(data => {
-    restaurantList = data.businesses;
-  });
-
 app.get('/api/random', (req, res) => {
-  selectedRestaurant = restaurantList[Math.floor(Math.random() * restaurantList.length)];
-
-  res.send(selectedRestaurant);
-});
-
-app.get('/api/random/1', (req, res) => {
-  const sql = `
-    select "restaurantId",
-      "name",
-      "image"
-    from "randomRestaurant"
-    where "restaurantId" = 1
-  `;
-
-  db.query(sql)
-    .then(result => {
-      res.status(201).json(result.rows[0]);
-    });
-});
-
-app.put('/api/random', (req, res) => {
-  const sql = `
-    update "randomRestaurant"
-      set "name" = $1,
-        "image" = $2
-    where "restaurantId" = 1
-    returning *
-  `;
-
-  const params = [selectedRestaurant.name, selectedRestaurant.image_url];
-  db.query(sql, params)
-    .then(result => {
-      const [random] = result.rows;
-      res.status(201).json(random);
+  fetch(`https://api.yelp.com/v3/businesses/search?term=${term}&location=${location}`, {
+    headers: {
+      'Content-Type': 'application/json',
+      Authorization: process.env.BEARER
+    }
+  })
+    .then(res => res.json())
+    .then(data => {
+      restaurantList = data.businesses;
+    })
+    .then(() => {
+      selectedRestaurant = restaurantList[Math.floor(Math.random() * restaurantList.length)];
+      res.send(selectedRestaurant);
     })
     .catch(err => {
       console.error(err);
-      res.status(500).json({
-        error: 'An unexpected error ocurred.'
-      });
     });
-
 });
 
 app.listen(process.env.PORT, () => {
